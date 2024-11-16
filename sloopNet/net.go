3b7e8b9559d93fb0514e979cb2b37ead9b671483
Cynthia Allen <cynthiaallen228@gmail.com>
2024-11-15 22:50:01 -0800

    rewrite sloopNet

--- sloopNet/net.go
+++ sloopNet/net.go
@@ -2,52 +2,12 @@ package main
 
 import (
 	"errors"
-	"fmt"
 	"strconv"
 	"strings"
 )
 
-type PositionalMsg struct {
-	msgContent string
-	row        uint8
-	col        uint8
-}
+func ParsePos(pos string) ([2]uint8, error) {
 
-func (m PositionalMsg) GetType() string {
-	return m.msgContent
-}
-
-type ShipMsg struct {
-	msgContent string
-	shipId     uint8
-}
-
-func (m ShipMsg) GetType() string {
-	return m.msgContent
-}
-
-type GameMsg struct {
-	msgContent string
-}
-
-func (m GameMsg) GetType() string {
-	return m.msgContent
-}
-
-type ConnMsg struct {
-	msgContent string
-}
-
-func (m ConnMsg) GetType() string {
-	return m.msgContent
-}
-
-type SloopMsg interface {
-	GetType() string
-}
-
-// see the documentation for positional message type in Battlesloop Protocol
-func parsePositional(message string) (PositionalMsg, error) {
 	var colMap = map[string]uint8{
 		"A": 0,
 		"B": 1,
@@ -61,104 +21,43 @@ func parsePositional(message string) (PositionalMsg, error) {
 		"J": 9,
 	}
 
-	// the return value which we will build up
-	var result PositionalMsg
-
-	// separate type from content
-	var parts []string = strings.Split(message, "_")
-	result.msgContent = parts[1]
-
-	// parse the content
-	var posStrs []string = strings.Split(parts[2], "-")
+	// split the string
+	var posStrs []string = strings.Split(pos, "-")
+	var result [2]uint8
 
 	// row
 	// This MUST be an integer
 	row, err := strconv.Atoi(posStrs[1])
 	if err != nil {
-		return PositionalMsg{}, err
+		return [2]uint8{}, err
 	}
 	// do bounds checking
 	if row < 0 || row > 9 {
-		return PositionalMsg{}, errors.New("Row number out of range")
+		return [2]uint8{}, errors.New("Row number out of range")
 	}
-	result.row = uint8(row)
+	result[0] = uint8(row)
 
 	// column
 	col, valid := colMap[posStrs[0]]
 	if !valid {
-		return PositionalMsg{}, errors.New("Invalid column identifier")
+		return [2]uint8{}, errors.New("Invalid column identifier")
 	}
-	result.col = col
+	result[1] = col
 
 	return result, nil
+
 }
 
-func parseShip(message string) (ShipMsg, error) {
-
-	var result ShipMsg
-
-	// separate type from content
-	var parts []string = strings.Split(message, "_")
-	result.msgContent = parts[1]
-
-	// parse the ship ID
-	val, err := strconv.Atoi(parts[2])
-	if err != nil {
-		return ShipMsg{}, err
-	}
-	result.shipId = uint8(val)
-
-	return result, nil
-}
-
-func parseGame(message string) (GameMsg, error) {
-	var result GameMsg
-
-	// separate type from content
-	var parts []string = strings.Split(message, "_")
-	result.msgContent = parts[2]
-
-	return result, nil
-}
-
-func parseConn(message string) (ConnMsg, error) {
-	var result ConnMsg
-
-	// separate type from content
-	var parts []string = strings.Split(message, "_")
-	result.msgContent = parts[2]
-
-	return result, nil
-}
-
-// func ParseMessage(msg string) SloopMsg {
-// 	var typeMap = map[rune]interface{} {
-// 		'h': parsePositional,
-// 		'm': parsePositional,
-// 		'f': parsePositional,
-// 		'a': parseShip,
-// 		'g': parseGame,
-// 		'c': parseConn,
-// 	}
-// 	// split msg based on ':' delimiters
-// 	var messages []string = strings.Split(msg, ":")
-//
-// 	// figure out what each part is
-// 	for _,m := range(messages) {
-// 		// separate the type from the contents
-// 		var parts = strings.Split(m, "_")
-//
-// 	}
-//
-// }
-
-func main() {
-	message := "_h_A-7"
-	result, err := parsePositional(message)
-	if err != nil {
-		fmt.Println(err)
-		return
+func ParseMsgs(message string) ([][]string, error) {
+	var msgs []string = strings.Split(message, ":")
+	var result [][]string
+	for i := range msgs {
+		var ss []string = strings.Split(msgs[i], "_")
+		if len(ss) != 3 {
+			return [][]string{}, errors.New("Invalid message: has no type or too many types")
+		}
+		result = append(result, ss[1:]) // first value will be "" because of the leading "_"
 	}
 
-	fmt.Println(result)
+	return result, nil
 }
--- sloopNet/net_test.go
+++ sloopNet/net_test.go
@@ -6,88 +6,103 @@ import (
 
 // TODO: make sure the parser can handle invalid message types
 
-func TestParsePositional(t *testing.T) {
+func msgEq(s1 [][]string, s2 [][]string) bool {
+	if len(s1) != len(s2) {
+		return false
+	}
+	for i := range s1 {
+		if len(s1) != len(s2) {
+			return false
+		}
+		for j := range s1[i] {
+			if s1[i][j] != s2[i][j] {
+				return false
+			}
+		}
+	}
 
-	/* Ensure parsePositional does the following things:
-	1. gives correct output on valid inputs, as defined in the Battlesloop Protocol spec
-	2. handles invalid row or column values
-	*/
-	var message string = "_h_A-7" // hit on A7
-	result, err := parsePositional(message)
-	expected := PositionalMsg{"h", 7, 0}
+	return true
+}
+
+func TestParsePos(t *testing.T) {
+	var message string = "A-7"
+	result, err := ParsePos(message)
+	expected := [2]uint8{7, 0}
 	if result != expected || err != nil {
-		t.Fatalf("parsePositional(%s) returned %v, %v | expected %v, %v", message, result, err, expected, nil)
+		t.Fatalf("ParsePos(%s) returned %v, %v | expected %v, %v", message, result, err, expected, nil)
 	}
 
-	message = "_m_B-5" // miss on B5
-	result, err = parsePositional(message)
-	expected = PositionalMsg{"m", 5, 1}
+	message = "B-5"
+	result, err = ParsePos(message)
+	expected = [2]uint8{5, 1}
 	if result != expected || err != nil {
-		t.Fatalf("parsePositional(%s) returned %v, %v | expected %v %v", message, result, err, expected, nil)
+		t.Fatalf("ParsePos(%s) returned %v, %v | expected %v, %v", message, result, err, expected, nil)
 	}
 
-	message = "_f_J-9" // fire at I9
-	result, err = parsePositional(message)
-	expected = PositionalMsg{"f", 9, 9}
+	message = "J-9"
+	result, err = ParsePos(message)
+	expected = [2]uint8{9, 9}
 	if result != expected || err != nil {
-		t.Fatalf("parsePositional(%s) returned %v, %v | expected %v %v", message, result, err, expected, nil)
+		t.Fatalf("ParsePos(%s) returned %v, %v | expected %v, %v", message, result, err, expected, nil)
 	}
 
-	message = "_f_f-9" // fire at f9 -- 'f' is an invalid column, so this should return an error
-	result, err = parsePositional(message)
-	expected = PositionalMsg{"", 0, 0}
+	message = "f-9" // 'f' is an invalid column, so this should return an error
+	result, err = ParsePos(message)
+	expected = [2]uint8{0, 0}
 	if result != expected || err == nil {
-		t.Fatalf("parsePositional(%s) returned %v, %v | expected %v Invalid column error", message, result, err, expected)
+		t.Fatalf("ParsePos(%s) returned %v, %v | expected %v Invalid column error", message, result, err, expected)
 	}
 
-	message = "_f_F-370" // fire at F370 -- row is out of range, so this should return an error
-	result, err = parsePositional(message)
-	expected = PositionalMsg{"", 0, 0}
+	message = "F-370" // fire at F370 -- row is out of range, so this should return an error
+	result, err = ParsePos(message)
+	expected = [2]uint8{0, 0}
 	if result != expected || err == nil {
-		t.Fatalf("parsePositional(%s) returned %v, %v | expected %v Row out of range error", message, result, err, expected)
+		t.Fatalf("ParsePos(%s) returned %v, %v | expected %v Row out of range error", message, result, err, expected)
 	}
 
-	message = "_f_F-A" // fire at FA -- A is not a valid row because it is not a number
-	result, err = parsePositional(message)
-	expected = PositionalMsg{"", 0, 0}
+	message = "F-A" // A is not a valid row because it is not a number
+	result, err = ParsePos(message)
+	expected = [2]uint8{0, 0}
 	if result != expected || err == nil {
-		t.Fatalf("parsePositional(%s) returned %v, %v | expected %v Atoi() invalid syntax error", message, result, err, expected)
+		t.Fatalf("ParsePos(%s) returned %v, %v | expected %v Atoi() invalid syntax error", message, result, err, expected)
 	}
 }
 
-func TestParseShip(t *testing.T) {
+func TestParseMsgs(t *testing.T) {
 
-	message := "_a_7" // sank ship 7
-	result, err := parseShip(message)
-	expected := ShipMsg{"a", 7}
-	if result != expected || err != nil {
-		t.Fatalf("parseShip(%s) returned %v, %v | expected %v, %v", message, result, err, expected, nil)
+	var message string = "_g_begin:_f_A-7"
+	result, err := ParseMsgs(message)
+	expected := [][]string{{"g", "begin"}, {"f", "A-7"}}
+	if !msgEq(expected, result) || err != nil {
+		t.Fatalf("ParseMsgs(%s) returned %v, %v | expected %v, %v", message, result, err, expected, nil)
 	}
 
-	message = "_a_g" // sank ship g -- illegal ship id, should return an error
-	result, err = parseShip(message)
-	expected = ShipMsg{}
-	if result != expected || err == nil {
-		t.Fatalf("parseShip(%s) returned %v, %v | expected %v, Atoi() invalid syntax error", message, result, err, expected)
+	message = "_g_begin"
+	result, err = ParseMsgs(message)
+	expected = [][]string{{"g", "begin"}}
+	if !msgEq(expected, result) || err != nil {
+		t.Fatalf("ParseMsgs(%s) returned %v, %v | expected %v, %v", message, result, err, expected, nil)
 	}
-}
 
-func TestParseGame(t *testing.T) {
-	// TODO: enforce better input sanitization
-	message := "_g_win"
-	result, err := parseGame(message)
-	expected := GameMsg{"win"}
-	if result != expected || err != nil {
-		t.Fatalf("parseGame(%s) returned %v, %v | expected %v, %v", message, result, err, expected, nil)
+	// some invalid messages
+	message = "begin"
+	result, err = ParseMsgs(message)
+	expected = [][]string{}
+	if !msgEq(expected, result) || err == nil {
+		t.Fatalf("ParseMsgs(%s) returned %v, %v | expected %v, Invalid message: has no type or too many types", message, result, err, expected)
 	}
-}
 
-func TestParseConn(t *testing.T) {
-	// TODO: enforce better input sanitization
-	message := "_c_begin"
-	result, err := parseConn(message)
-	expected := ConnMsg{"begin"}
-	if result != expected || err != nil {
-		t.Fatalf("parseGame(%s) returned %v, %v | expected %v, %v", message, result, err, expected, nil)
+	message = "_g__f_begin"
+	result, err = ParseMsgs(message)
+	expected = [][]string{}
+	if !msgEq(expected, result) || err == nil {
+		t.Fatalf("ParseMsgs(%s) returned %v, %v | expected %v, Invalid message: has no type or too many types", message, result, err, expected)
+	}
+
+	message = "_g_begin:"
+	result, err = ParseMsgs(message)
+	expected = [][]string{}
+	if !msgEq(expected, result) || err == nil {
+		t.Fatalf("ParseMsgs(%s) returned %v, %v | expected %v, Invalid message: has no type or too many types", message, result, err, expected)
 	}
 }
